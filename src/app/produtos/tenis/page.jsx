277c2ebed942d83b841c5f6f5a24903b0678e372
MatheusFelipeCamarinhaDuarte <rm552295@fiap.com.br>
2023-10-17 09:56:52 -0300

    adicção de rotas

---
+++ src/app/produtos/tenis/page.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Tenis() {
+  return (
+    <div>
+        <h1>Tênis</h1>
+        <div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus est fugit nihil quidem cumque eum labore molestiae, atque perferendis minima ipsum architecto nam autem maxime necessitatibus non. Delectus, voluptates dolorum.</p>
+            <p><Link href="/"> Voltar...</Link></p>
+        </div>
+    </div>
+  )
+}
