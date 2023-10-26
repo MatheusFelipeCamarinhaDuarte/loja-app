ffb3ddfb87ed522b0f97fb4656224398d082e7b0
MatheusFelipeCamarinhaDuarte <rm552295@fiap.com.br>
2023-10-26 09:36:22 -0300

    APIs locais - consumo e distribuição. imagens externas

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -5,6 +5,7 @@ export default function Cabecalho() {
     return (
         <header className="bg-gray-800 text-white h-16">
             <nav className="flex flex-row justify-around">
+                <Link className="link-cabecalho" href="/consumo/eletro-view">Eletrônicos</Link>
                 <Link className="link-cabecalho" href="/produtos/produto/estaticas/tenis">TÊNIS</Link>
                 <Link className="link-cabecalho" href="/produtos/produto/dinamicas/estranho/tenis-preto">TÊNIS PRETO</Link>
                 <Link className="link-cabecalho" href="/produtos/produto/dinamicas/vestuario/camisa-xadrez">CAMISA-X</Link>
