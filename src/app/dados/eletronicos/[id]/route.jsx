2c48b117921524272b0e0f9148397bca787bad98
MatheusFelipeCamarinhaDuarte <rm552295@fiap.com.br>
2024-05-17 14:45:52 -0300

    api

--- src/app/dados/eletronicos/[id]/route.jsx
+++ src/app/dados/eletronicos/[id]/route.jsx
@@ -6,14 +6,14 @@ import fs from "fs/promises";
         const file = await fs.readFile(process.cwd() + '/src/app/dados/base/db.json', 'utf8');
         const arquivo = JSON.parse(file);
         const eletros = arquivo.eletros;         
-
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL
         const id = params.id
 
         if(id > 0 && id <= eletros.length){
             return NextResponse.json(eletros.find((eletro=> eletro.id == id)));
         } else {
             // Se o ID for inválido ou não existir no array, retorna um erro HTTP 404
-            return id == 0 ? NextResponse.json(eletros) : NextResponse.redirect("http://localhost:3000/error") 
+            return id == 0 ? NextResponse.json(eletros) : NextResponse.redirect(`${apiUrl}/error`) 
         }
 
 
